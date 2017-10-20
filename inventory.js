d96e189bafbf594783aae2733848707be53010c5
Greg Lawrence <gregaudio@gmail.com>
2017-10-20 15:17:59 -0500

    added more comments about my method to create class names

--- inventory.js
+++ inventory.js
@@ -18,6 +18,13 @@ for (var key in HomeInventory) {
         var item = categoryArray[i];
 
     // hardcoded the __name to avoid another for in loop to access property names
+    // Check other branch titled "tryingAnotherNestedForInLoop" on github to see 
+    // my other method for accessing property names. In that branch I used another 
+    // loop and assigned property names to variables and placed variables in my 
+    // template string. For this application that extra step doesn't seem needed 
+    // since we know the info we want from object and what class name we want to
+    // match it with. 
+
         inventoryEl.innerHTML += `
         <section class="${item.type}">
             <h2 class="${item.type}__name">
